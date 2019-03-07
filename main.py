525be74fc239659e474f387853336fb7d0fae510
Michael Jacob <46906037+mjac0b@users.noreply.github.com>
2019-03-07 22:07:29 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#File describing name of one of my favourite songs at the moment
+
+#Artist states the name of the artist
+Artist = "Eben"
+
+#name of the song
+Song = "At the centre of it all"
+
+#the genre of the song/artist
+Genre = "Gospel"
+
+#length of the song
+DurationInSeconds = 320
+
+#year the song was released
+Year = 2018
+
+#album of the song
+Album = "Untitled"
+
+print(Artist)
+print(Song)
+print(DurationInSeconds)
+print(Genre)
+print(Year)
+print(Album)
